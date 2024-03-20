9adbeb91956d44d94cfa82d15111cd579d948f79
Pedro Castro <contatoaugustopedro@gmail.com>
2024-03-20 13:29:57 -0300

    declaring winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -8,7 +8,7 @@ function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquare = squares.slice();
@@ -21,8 +21,17 @@ function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div>{status}</div>
       <div>
         <Square value={squares[0]} onSquareclick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareclick={() => handleClick(1)} />
@@ -40,6 +49,26 @@ function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
